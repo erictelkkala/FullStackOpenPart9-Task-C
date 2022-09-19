941bf9dcf9748dc2d42d23a9e2e971e5f5f10fd5
erictelkkala <eric.telkkala@gmail.com>
2022-09-19 14:49:29 +0300

    Task 9.10

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnose';
 const app = express();
 app.use(express.json());
 
@@ -8,6 +9,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+// Routes for diagnoses
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ src/routes/diagnose.ts
@@ -0,0 +1,16 @@
+import diagnosesData from '../../data/diagnoses.json';
+import Diagnose from '../types';
+import express from 'express';
+
+const router = express.Router();
+
+// Return all the diagnoses
+const getEntries = (): Array<Diagnose> => {
+    return diagnosesData;
+};
+
+router.get('/', (_req, res) => {
+    res.send(getEntries());
+});
+
+export default router;
